5e34f9cae3b2d78e41242c0b1a2a993ac62fb6a7
nkdem <dev@nkdem.net>
2025-04-21 12:54:16 +0100

    initial commit

---
+++ eleventy.config.js
@@ -0,0 +1,27 @@
+import htmlmin from "html-minifier-terser"
+const now = String(Date.now())
+/** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
+export default async function(eleventyConfig) {
+    eleventyConfig.setInputDirectory("src")
+    eleventyConfig.setLayoutsDirectory("layouts")
+    eleventyConfig.setOutputDirectory("dist")
+
+    // allows hot reloading when tailwind reloads
+    eleventyConfig.setServerOptions({
+        watch: ["dist/style.css"]
+    })
+
+    // inspired from https://www.11ty.dev/docs/transforms/#minify-html-output
+    eleventyConfig.addTransform("htmlmin", async function (content) {
+        // Only minify if in PRODUCTION and if an HTML output
+        if (process.env.ELEVENTY_PRODUCTION && (this.page.outputPath || "").endsWith(".html")) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            })
+            return minified
+        }
+        return content
+    })
+}
---
+++ src/styles/tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    content: ['dist/**/*.html'],
+    safelist: [],
+    plugins: [], 
+}
